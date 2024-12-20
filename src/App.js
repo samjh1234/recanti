5dbe9f3fb4d2e5e648494023d17a200965309ea5
samjh1234 <wisamjh@hotmail.com>
2024-12-20 09:09:23 +0000

    added manifest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Aggiungi from './components/Aggiungi';
 
 function App() {
   return (
-    <Router basename="/recanti">
+    <Router basename={process.env.PUBLIC_URL}> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aggiungi" element={<Aggiungi />} />
