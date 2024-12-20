f19c0d4233f40eeca763011255364db1df63c536
samjh1234 <wisamjh@hotmail.com>
2024-12-20 10:48:20 +0000

    commin

--- src/db/dexie.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-const db = new Dexie("LyricsDatabase");
-
-db.version(2).stores({
-  lyrics: "++id, category, title, text, notes, photo, photoType, audio, audioType, doc, docType"
-});
-
-export default db;
